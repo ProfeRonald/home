9de90b40dc1adf6aeea7a07d32bda78f9ef4d286
ProfeRonald <r.ronald@gmail.com>
2026-09-10 19:07:34 -0400

    fix(home): use provider brand logos

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'hogar-public-263b826f76363467';
-const PUBLIC_ASSETS = ["./assets/browser-CqDbEFy1.js","./assets/index-B16yS5B7.css","./assets/index-D9std1Z8.js","./assets/index-WPj-8i4L.js","./assets/index-iPjrJSYw.js","./assets/index.esm-CPzTFSy8.js"];
+const CACHE = 'hogar-public-020bcac096aa44b1';
+const PUBLIC_ASSETS = ["./assets/browser-CqDbEFy1.js","./assets/index-CI1e2DNM.js","./assets/index-D9std1Z8.js","./assets/index-DdH6muuY.css","./assets/index-iPjrJSYw.js","./assets/index.esm-MelSI07G.js"];
 const base = new URL('./', self.location);
 const shell = new URL('./index.html', base).href;
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll([shell, './icon-192.png', './icon-512.png', './person.jpg', ...PUBLIC_ASSETS]))));
